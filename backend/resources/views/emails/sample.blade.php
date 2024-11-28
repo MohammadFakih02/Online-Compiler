d5bc07637c51d1d5b6fa0d28b6301a68622b4c54
Moe-Karim <mohamadkarimfahs@gmail.com>
2024-11-28 13:03:09 +0200

    Created:Mail view

---
+++ backend/resources/views/emails/sample.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Email</title>
+</head>
+<body>
+    <h1>Hello,</h1>
+    <p>I’ve shared a file titled with you and would love your input. You can access it here:<a href="http://localhost:3000/"> Collaborate </a>.
+        <br>Please feel free to review, edit, or share feedback.<br>
+         Let me know if you have any issues accessing the file.
+        <br>
+        Thank you!
+        <br>
+        Best regards,
+        {{$name}}</p>
+</body>
+</html>
